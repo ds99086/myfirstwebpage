72a823139ee5d67330782175afb4d7423623212a
ds99086 <ds99086@gmail.com>
2021-04-29 20:58:19 +1200

    try using cards for words

---
+++ public/definitiondb.js
@@ -0,0 +1,11 @@
+let terminologies = [
+    {"word": "DNA", "definition": "Deoxyribonucleic Acid", "topic":"Genetics", },
+    {"word": "Heterozygous", "definition": "A pair of object made up of different material", "topic":"Genetics", },
+    {"word": "Homozygous", "definition": "A pair of object made up of same material", "topic":"Genetics", },
+    {"word": "Dominant allele", "definition": "An allele that is always expressed in phenotype", "topic":"Genetics", },
+    {"word": "Recessive allele", "definition": "An allele that requires two copies to be expressed in phenotype", "topic":"Genetics", },
+
+];
+
+// an array of objects. 
+//loop through the objets calculate the statistics. 
---
+++ public/wordlist.js
@@ -0,0 +1,24 @@
+window.addEventListener("load", function() {
+ 
+
+
+
+
+    let wordColumn = document.querySelector("#geneticsWordList");
+
+    for (let i = 0; i < terminologies.length; i++) {
+        term =  terminologies[i];
+        wordColumn.innerHTML +=
+        `<div class="card">
+        <img class="card-img-top" src="https://medlineplus.gov/images/PX00006G_PRESENTATION.jpeg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${term.word}</h5>
+          <p class="card-text">${term.definition}</p>
+        </div>
+      </div>`;
+        
+    } 
+    
+
+
+});
