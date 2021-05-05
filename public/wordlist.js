c3262522dc5aee5b407896c1e2d856aae3c9bcc0
Allie Lim <slim287@UoA.auckland.ac.nz>
2021-05-05 16:18:05 +1200

    More effects when hovering. Make an option to let kids practice with definition to get words.

--- public/wordlist.js
+++ public/wordlist.js
@@ -12,7 +12,7 @@ window.addEventListener("load", function() {
         `<div class="card">
         <img class="card-img-top" src="https://medlineplus.gov/images/PX00006G_PRESENTATION.jpeg" alt="Card image cap">
         <div class="card-body">
-          <h5 class="card-title">${term.word}</h5>
+          <h4 class="card-title">${term.word}</h4>
           <p class="card-text">${term.definition}</p>
         </div>
       </div>`;
